6f326dcaf7ac1ecf2a4a2d57f06e0ae6e2d9e24a
Aditya <aditya_garg@horacemann.org>
2025-10-12 13:00:18 -0400

    Fixing minor bugs

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # Scroll-Art-Project
 Scroll art for Intermediate Com Sci
+Citation points: 
 movingSymbols was created only by me, citation point of 1. It is a design that expands and contracts as time goes on. 
 The ascii spiders were also created by me, so only a citation point of 1 is generated. 
 The boxes around the spiders can also be considered art, and they also only generate 1 citation point. 
+
+Design of spider is included, this is the brainstorm of the spider ascii. 
--- asciiArt.java
+++ asciiArt.java
@@ -20,33 +20,45 @@ public class asciiArt{
         // same shape as before
         spider[0][4] = '('; spider[0][5] = '('; spider[0][6] = '(';
         spider[0][20] = ')'; spider[0][21] = ')'; spider[0][22] = ')';
+
         spider[1][4] = '('; spider[1][5] = '('; spider[1][6] = '('; spider[1][7] = '(';
         spider[1][19] = ')'; spider[1][20] = ')'; spider[1][21] = ')'; spider[1][22] = ')';
+
         spider[2][4] = '('; spider[2][5] = '('; spider[2][6] = '('; spider[2][7] = '(';
         spider[2][19] = ')'; spider[2][20] = ')'; spider[2][21] = ')'; spider[2][22] = ')';
+
         spider[3][5] = '('; spider[3][6] = '('; spider[3][7] = '(';
         spider[3][11] = 'o'; spider[3][12] = 'o'; spider[3][13] = 'o'; spider[3][14] = 'o'; spider[3][15] = 'o';
         spider[3][19] = ')'; spider[3][20] = ')'; spider[3][21] = ')';
+
         spider[4][6] = '('; spider[4][7] = '(';
         spider[4][10] = 'o'; spider[4][11] = 'O'; spider[4][12] = 'O'; spider[4][13] = 'O'; spider[4][14] = 'O'; spider[4][15] = 'O'; spider[4][16] = 'o';
         spider[4][20] = ')'; spider[4][21] = ')';
+
         spider[5][6] = '(';
         spider[5][9] = 'o'; spider[5][10] = 'o'; spider[5][11] = 'O'; spider[5][12] = '@'; spider[5][13] = '@'; spider[5][14] = '@'; spider[5][15] = 'O'; spider[5][16] = 'o'; spider[5][17] = 'o';
         spider[5][21] = ')';
+
         spider[6][9] = 'o'; spider[6][10] = 'O'; spider[6][11] = '@'; spider[6][12] = '@'; spider[6][13] = '@'; spider[6][14] = '@'; spider[6][15] = '@'; spider[6][16] = 'O'; spider[6][17] = 'o';
+        
         spider[7][6] = '(';
         spider[7][9] = 'o'; spider[7][10] = 'o'; spider[7][11] = 'O'; spider[7][12] = '@'; spider[7][13] = '@'; spider[7][14] = '@'; spider[7][15] = 'O'; spider[7][16] = 'o'; spider[7][17] = 'o';
         spider[7][21] = ')';
+
         spider[8][6] = '('; spider[8][7] = '(';
         spider[8][10] = 'o'; spider[8][11] = 'O'; spider[8][12] = 'O'; spider[8][13] = 'O'; spider[8][14] = 'O'; spider[8][15] = 'O'; spider[8][16] = 'o';
         spider[8][20] = ')'; spider[8][21] = ')';
+        
         spider[9][5] = '('; spider[9][6] = '('; spider[9][7] = '(';
         spider[9][11] = 'o'; spider[9][12] = 'o'; spider[9][13] = 'o'; spider[9][14] = 'o'; spider[9][15] = 'o';
         spider[9][19] = ')'; spider[9][20] = ')'; spider[9][21] = ')';
+        
         spider[10][4] = '('; spider[10][5] = '('; spider[10][6] = '('; spider[10][7] = '(';
         spider[10][19] = ')'; spider[10][20] = ')'; spider[10][21] = ')'; spider[10][22] = ')';
+        
         spider[11][4] = '('; spider[11][5] = '('; spider[11][6] = '('; spider[11][7] = '(';
         spider[11][19] = ')'; spider[11][20] = ')'; spider[11][21] = ')'; spider[11][22] = ')';
+        
         spider[12][4] = '('; spider[12][5] = '('; spider[12][6] = '(';
         spider[12][20] = ')'; spider[12][21] = ')'; spider[12][22] = ')';
 
