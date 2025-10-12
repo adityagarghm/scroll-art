c704e78133869600f4fd32f87995663ba11c0343
Aditya <aditya_garg@horacemann.org>
2025-10-11 22:51:43 -0400

    Line increases thickness each bounce, before decreasing again

--- movingSymbols.java
+++ movingSymbols.java
@@ -7,38 +7,58 @@ public class movingSymbols{
         int[] right = new int[lines];
 
         // Start the animation loop forever
+        int counter = 0;
         while (true) {
-            moveIn(left, right, width, lines);   // stars move toward center
-            moveOut(left, right, width, lines);  // stars move back outward
+            moveIn(left, right, width, lines, counter);   // stars move toward center
+            moveOut(left, right, width, lines, counter);  // stars move back outward
+            counter++;
         }
     }
 
     // Method 1: move stars inward
-    public static void moveIn(int[] left, int[] right, int width, int lines) throws InterruptedException {
+    public static void moveIn(int[] left, int[] right, int width, int lines, int counter) throws InterruptedException {
         for (int i = 0; i < lines; i++) {
             left[i] = i;
             right[i] = width - 1 - i;
-            printLine(width, left[i], right[i]); 
-            Thread.sleep(20);
+            printLine2(width, left[i], right[i], counter);
+            Thread.sleep(20); 
         }
     }
 
     // Method 2: move stars outward
-    public static void moveOut(int[] left, int[] right, int width, int lines) throws InterruptedException {
+    public static void moveOut(int[] left, int[] right, int width, int lines, int counter) throws InterruptedException {
         for (int i = lines - 1; i >= 0; i--) {
             left[i] = i;
             right[i] = width - 1 - i;
-            printLine(width, left[i], right[i]);
+            printLine2(width, left[i], right[i], counter);
             Thread.sleep(20);
         }
     }
 
     // Method 3: print one line of stars
-    public static void printLine(int width, int left, int right) {
+    /*public static void printLine(int width, int left, int right, int counter) {
         char[] line = new char[width];
         for (int j = 0; j < width; j++) line[j] = ' ';
         line[left] = '*';
         line[right] = '*';
+        System.out.println(new String(line));
+    }*/
+    //method 4 - adding to pattern
+    public static void printLine2(int width, int left, int right, int counter) {
+        char[] line = new char[width];
+        for (int j = 0; j < width; j++) line[j] = ' ';
+        if (counter%13 <=7){//get larger
+        for (int step_increase = 0; step_increase <= counter%13; step_increase++){
+        line [left + step_increase] = '*';
+        line[right - step_increase] = '*';
+        }
+        }else{//get smaller
+        for (int step_increase = 13 - counter%13; step_increase > 0; step_increase--){
+            line [left + step_increase] = '*';
+            line[right - step_increase] = '*';
+        }
+        }
+
         System.out.println(new String(line));
     }
 }
