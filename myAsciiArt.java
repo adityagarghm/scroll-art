975f600990d058fd3c0f72adc2f2e6acf5edd074
Aditya <aditya_garg@horacemann.org>
2025-10-27 09:37:47 -0400

    Added ascii art

--- myAsciiArt.java
+++ myAsciiArt.java
@@ -1,8 +1,14 @@
-public class asciiArt{
+import java.util.Random;
+public class myAsciiArt{
+
     public static void main(String[] args) throws InterruptedException{
+        AsciiArt[] images = {new AsciiArt(makeSpider()), new AsciiArt(getBoat())}; 
     while (true){
-       char[][] spider = makeSpider();       // build the spider shape
-        printRowOfSpiders(spider, 4);   // print 4 spiders side by side
+        Random r = new Random();
+        int x = r.nextInt(0,3);
+       //char[][] img = images[0].getImg();       
+        printRowOfSpiders(images[x], 4);   // print 4 spiders side by side
+
         }     
     }
 
@@ -17,7 +23,7 @@ public class asciiArt{
             }
         }
 
-        // same shape as before
+        // spider shape
         spider[0][4] = '('; spider[0][5] = '('; spider[0][6] = '(';
         spider[0][20] = ')'; spider[0][21] = ')'; spider[0][22] = ')';
 
@@ -64,11 +70,171 @@ public class asciiArt{
 
         return spider;
     }
+    static char[][] getBoat() {
+        char[][] img = new char[10][34];
+        // fill with empty space
+        for (int y = 0; y < 10; y++) {
+            for (int x = 0; x < 34; x++) {
+                img[y][x] = ' ';
+            }
+        }
+        // then fill individual characters
+        img[0][15] = '|';
+        img[0][16] = '_';
+        img[0][17] = '_';
+        img[0][18] = '_';
+        img[0][19] = '|';
+
+        img[1][4] = '*';
+        img[1][15] = '|';
+        img[1][16] = '_';
+        img[1][17] = '_';
+        img[1][18] = '_';
+        img[1][19] = '|';
+        img[1][21] = '|';
+        img[1][22] = '\\';
+        img[1][26] = '*';
+        img[1][28] = '*';
+
+        img[2][3] = '*';
+        img[2][9] = '_';
+        img[2][10] = '_';
+        img[2][11] = '|';
+        img[2][12] = '_';
+        img[2][13] = '_';
+        img[2][15] = '|';
+        img[2][16] = '_';
+        img[2][17] = '_';
+        img[2][18] = '_';
+        img[2][19] = '|';
+        img[2][21] = '|';
+        img[2][23] = '\\';
+        img[2][27] = '*';
+
+        img[3][5] = '*';
+        img[3][9] = '|';
+        img[3][10] = 'o';
+        img[3][11] = '_';
+        img[3][12] = '_';
+        img[3][13] = '|';
+        img[3][15] = '|';
+        img[3][16] = '_';
+        img[3][17] = '_';
+        img[3][18] = '_';
+        img[3][19] = '|';
+        img[3][21] = '|';
+        img[3][22] = 'o';
+        img[3][24] = '\\';
+
+        img[4][9] = '|';
+        img[4][10] = '_';
+        img[4][11] = '_';
+        img[4][12] = '_';
+        img[4][13] = '|';
+        img[4][15] = '|';
+        img[4][16] = '_';
+        img[4][17] = '_';
+        img[4][18] = '_';
+        img[4][19] = '|';
+        img[4][21] = '|';
+        img[4][22] = '_';
+        img[4][23] = '_';
+        img[4][24] = 'o';
+        img[4][25] = '\\';
+
+        img[5][1] = '^';
+        img[5][2] = '^';
+        img[5][8] = '_';
+        img[5][9] = '|';
+        img[5][10] = '_';
+        img[5][11] = '_';
+        img[5][12] = '_';
+        img[5][13] = '|';
+        img[5][17] = '|';
+        img[5][21] = '|';
+        img[5][30] = '^';
+        img[5][31] = '^';
+        img[5][32] = '^';
+        
+        img[6][2] = '^';
+        img[6][3] = '^';
+        img[6][4] = '^';
+        img[6][7] = '/';
+        img[6][8] = '.';
+        img[6][9] = '.';
+        img[6][10] = '.';
+        img[6][11] = '\\';
+        img[6][12] = '_';
+        img[6][13] = '_';
+        img[6][14] = '_';
+        img[6][15] = '|';
+        img[6][16] = '_';
+        img[6][17] = '|';
+        img[6][18] = '_';
+        img[6][19] = '_';
+        img[6][20] = '_';
+        img[6][21] = '|';
+        img[6][22] = '_';
+        img[6][23] = '_';
+        img[6][24] = '_';
+        img[6][25] = '_';
+        img[6][26] = '\\';
+        img[6][27] = '_';
+        img[6][28] = '/';
+        img[6][32] = '^';
+        img[6][33] = '^';
+
+        img[7][0] = '~';
+        img[7][1] = '~';
+        img[7][2] = '~';
+        img[7][3] = '~';
+        img[7][4] = '~';
+        img[7][5] = '~';
+        img[7][6] = '~';
+        img[7][7] = '\\';
+        img[7][11] = 'o';
+        img[7][13] = '*';
+        img[7][15] = 'o';
+        img[7][17] = '*';
+        img[7][19] = '*';
+        img[7][21] = 'o';
+        img[7][23] = 'o';
+        img[7][26] = '/';
+        img[7][27] = '~';
+        img[7][28] = '~';
+        img[7][29] = '~';
+        img[7][30] = '~';
+        img[7][31] = '~';
+        img[7][32] = '~';
+        img[7][33] = '~';
+
+        img[8][17] = '^';
+        img[8][18] = '^';
+        img[8][19] = '^';
+        img[8][20] = '^';
+        img[8][21] = '^';
+        img[8][22] = '^';
+        img[8][23] = '^';
+
+        img[9][3] = '^';
+        img[9][4] = '^';
+        img[9][5] = '^';
+        img[9][11] = '^';
+        img[9][12] = '^';
+        img[9][13] = '^';
+        img[9][27] = '^';
+        img[9][28] = '^';
+        img[9][29] = '^';
+
+        return img;
+    }
 
     // prints multiple spiders side by side, each inside a box of *
-    public static void printRowOfSpiders(char[][] spider, int spiderCount) throws InterruptedException {
-        int height = spider.length;          // number of rows in one spider
-        int width = spider[0].length;        // number of columns in one spider
+    public static void printRowOfSpiders(AsciiArt spider, int spiderCount) throws InterruptedException {
+        int height = spider.height;          // number of rows in one spider
+        int width = spider.width;   // number of columns in one spider
+        double unroundedSpidercount =  120/width;     
+        long spidercount = Math.round(unroundedSpidercount);
 
         // loop through each row (plus one top and one bottom border)
         for (int rowCount = 0; rowCount < height + 2; rowCount++) {
@@ -82,7 +248,7 @@ public class asciiArt{
                     System.out.print('*');
                     // print spider contents for this row
                     for (int columnSpider = 0; columnSpider < width; columnSpider++) {
-                        System.out.print(spider[rowCount - 1][columnSpider]);  
+                        System.out.print(spider.img[rowCount - 1][columnSpider]);  
                     }
                     // print right border
                     System.out.print('*');
